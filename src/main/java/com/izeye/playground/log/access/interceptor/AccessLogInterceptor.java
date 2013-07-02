b89e5d06cb57221073150f82076c7c70ce7aa254
izeye <izeye@naver.com>
2013-07-03 01:42:04 +0900

    Refactoring.

--- src/main/java/com/izeye/playground/log/access/interceptor/AccessLogInterceptor.java
+++ src/main/java/com/izeye/playground/log/access/interceptor/AccessLogInterceptor.java
@@ -13,22 +13,17 @@ import com.izeye.playground.log.access.service.AccessLogService;
 @Component("accessLogInterceptor")
 public class AccessLogInterceptor extends HandlerInterceptorAdapter {
 
-	private static final String URI_PREFIX_RESOURCES = "/resources/";
-
 	@Resource
 	private AccessLogService accessLogService;
 
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
-		String requestURI = request.getRequestURI();
-		if (!requestURI.startsWith(URI_PREFIX_RESOURCES)) {
-			String ip = request.getRemoteAddr();
-			String url = request.getRequestURL().toString();
-			String userAgent = HttpUtils.getUserAgent(request);
-			String referer = HttpUtils.getReferer(request);
-			accessLogService.log(ip, url, userAgent, referer);
-		}
+		String ip = request.getRemoteAddr();
+		String url = request.getRequestURL().toString();
+		String userAgent = HttpUtils.getUserAgent(request);
+		String referer = HttpUtils.getReferer(request);
+		accessLogService.log(ip, url, userAgent, referer);
 		return super.preHandle(request, response, handler);
 	}
 
